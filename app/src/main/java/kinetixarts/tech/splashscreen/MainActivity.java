252d716b5ded4042f2efafac9b01df40e00f3715
kinetix <kinetixarts@outlook.com>
2020-04-10 10:20:52 +0300

    init android files commit

---
+++ app/src/main/java/kinetixarts/tech/splashscreen/MainActivity.java
@@ -0,0 +1,14 @@
+package kinetixarts.tech.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
