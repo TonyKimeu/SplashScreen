40325c8a230921c2ed4263489ab8756c6599b028
kinetix <kinetixarts@outlook.com>
2020-04-10 11:06:42 +0300

    Splashscreen snippet

--- app/src/main/java/kinetixarts/tech/splashscreen/App.java
+++ app/src/main/java/kinetixarts/tech/splashscreen/App.java
@@ -1,4 +1,17 @@
 package kinetixarts.tech.splashscreen;
 
-public class App {
+import android.app.Application;
+import android.os.SystemClock;
+
+import java.util.concurrent.TimeUnit;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Don't do this! This is just so cold launches take some time
+        SystemClock.sleep(TimeUnit.SECONDS.toMillis(3));
+    }
 }
